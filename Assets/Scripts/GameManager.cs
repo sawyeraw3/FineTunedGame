b3debdd8ac2fd2a10fe8e7db0c9a1ce13f9cf8d5
sawyeraw3 <sawyer.warden@gmail.com>
2017-01-21 18:14:03 -0500

    Helpful managers

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject enemy;
+	public Transform[] spawnPoints;
+	public int maxEnemies;
+	public int secBetweenSpawn = 5;
+	public int waveEnemyIncrease;
+	int totalEnemies = 0;
+	int enemiesSpawned = 0;
+	int whichSpawn = 0;
+	float timer;
+
+	// Use this for initialization
+	void Start () {
+		totalEnemies += maxEnemies;
+		timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+
+		if (enemiesSpawned < maxEnemies && timer > secBetweenSpawn) {
+			SpawnEnemies ();
+			timer = 0;
+		} else if (KillManager.kills == totalEnemies) {
+			enemiesSpawned = 0;
+			maxEnemies += waveEnemyIncrease;
+			totalEnemies += maxEnemies;
+			WaveManager.wave ++;
+		}
+	}
+
+	void SpawnEnemies()
+	{
+		if (whichSpawn == spawnPoints.Length) {
+			whichSpawn = 0;
+		}
+		Transform.Instantiate (enemy, spawnPoints [whichSpawn].transform.position, spawnPoints [whichSpawn].transform.rotation);
+		whichSpawn ++;
+		enemiesSpawned ++;
+	}
+}
---
+++ Assets/Scripts/KillManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class KillManager : MonoBehaviour {
+	
+	public static int kills;        // The player's score.
+	
+	
+	Text text;                      // Reference to the Text component.
+	
+	
+	void Awake ()
+	{
+		// Set up the reference.
+		text = GetComponent <Text> ();
+		
+		// Reset the score.
+		kills = 0;
+	}
+	
+	
+	void Update ()
+	{
+		// Set the displayed text to be the word "Score" followed by the score value.
+		text.text = "Kills" + kills;
+	}
+}
