6a89d6bc2c2ebc97a2c43c710a60c9275f806a65
sawyeraw3 <sawyer.warden@gmail.com>
2017-01-22 03:30:31 -0500

    Gotchya

---
+++ Assets/Scripts/EnemyVars.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyVars : MonoBehaviour {
+
+	public GameObject[] ps;
+	public List<GameObject> pylons = new List<GameObject>();
+	public bool allPylonsDestroyed;
+
+	// Use this for initialization
+	void Start () {
+		allPylonsDestroyed = false;
+
+		ps = GameObject.FindGameObjectsWithTag ("Pylon");
+
+		foreach (GameObject go in ps) {
+			pylons.Add (go);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ps = GameObject.FindGameObjectsWithTag ("Pylon");
+		if (ps.Length == 0)
+			allPylonsDestroyed = true;
+	}
+}
