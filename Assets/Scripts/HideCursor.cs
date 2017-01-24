81fdf05edaa315fd564fc2e076ec0d5bc1c61175
sawyeraw3 <sawyer.warden@gmail.com>
2017-01-24 16:23:21 -0500

    Hides cursor

---
+++ Assets/Scripts/HideCursor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideCursor : MonoBehaviour {
+
+	public bool hideCursor;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.visible = !hideCursor;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
